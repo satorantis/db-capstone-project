1c8ede6242d631f4f6d07c27431c039ea8284e8d
satorantis <oksana.m@gmx.at>
2025-08-22 08:27:25 +0200

    changes

--- db_capstone_project.sql
+++ db_capstone_project.sql
@@ -1,2 +1,19 @@
-SHOW DATABASES;
-
+INSERT INTO MenuItems (MenuItemID, CourseName, Type, Price)
+VALUES
+(1, 'Olives','Starters',5),
+(2, 'Flatbread','Starters', 5),
+(3, 'Minestrone', 'Starters', 8),
+(4, 'Tomato bread','Starters', 8),
+(5, 'Falafel', 'Starters', 7),
+(6, 'Hummus', 'Starters', 5),
+(7, 'Greek salad', 'Main Courses', 15),
+(8, 'Bean soup', 'Main Courses', 12),
+(9, 'Pizza', 'Main Courses', 15),
+(10, 'Greek yoghurt','Desserts', 7),
+(11, 'Ice cream', 'Desserts', 6),
+(12, 'Cheesecake', 'Desserts', 4),
+(13, 'Athens White wine', 'Drinks', 25),
+(14, 'Corfu Red Wine', 'Drinks', 30),
+(15, 'Turkish Coffee', 'Drinks', 10),
+(16, 'Turkish Coffee', 'Drinks', 10),
+(17, 'Kabasa', 'Main Courses', 17);
